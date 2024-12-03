6341822c4da4c2528fa9918adb3b38f928431911
Oleksandr Akhobadze <sandoahobadze@gmail.com>
2024-12-03 23:18:43 +0100

    adding tests to test_restore_names.py

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,53 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def person_template() -> list:
+    return [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+
+def test_with_name_none(person_template: list) -> None:
+    restore_names(person_template)
+    expected = [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    assert person_template == expected
+
+
+def test_with_name(person_template: list) -> None:
+    person_template[0]["first_name"] = "Jack"
+    person_template[1]["first_name"] = "Mike"
+    restore_names(person_template)
+    expected = [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    assert person_template == expected
